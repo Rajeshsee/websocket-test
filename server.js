f5fa91a1ab189f6be98bb0ebb371e35709f84326
Rajesh Sardar <business.rajesh99@gmail.com>
2026-09-13 08:52:13 +0530

    Refactor WebSocket server setup and messages

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
 
-const port = process.env.PORT || 10000;
+const app = express();
 
-const wss = new WebSocket.Server({ port });
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({
+    server: server
+});
+
+app.get("/", (req, res) => {
+    res.send("WebSocket server is running!");
+});
 
 wss.on("connection", (ws) => {
+
     console.log("Client connected");
 
-    ws.send("Hello from Render WebSocket!");
+    ws.send("Hello from Render!");
 
     ws.on("message", (message) => {
+
         console.log("Received:", message.toString());
 
-        ws.send("Server received: " + message.toString());
+        ws.send(
+            "Server received: " + message.toString()
+        );
     });
 
     ws.on("close", () => {
         console.log("Client disconnected");
     });
+
+    ws.on("error", (error) => {
+        console.log("WebSocket error:", error);
+    });
 });
 
-console.log("WebSocket server running on port " + port);
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port " + PORT);
+});
